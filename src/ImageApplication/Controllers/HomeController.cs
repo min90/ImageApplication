255fc5a3d7f84181c6992fab501b90dc0c9cabc7
min90 <jesso12@student.sdu.dk>
2016-04-05 22:33:21 +0200

    First Commit

---
+++ src/ImageApplication/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ImageApplication.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ImageApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+         public IActionResult Index()
+         {
+            User user = new User { UserID = 1, UserName = "Jesper" };
+            return View(user);
+         }
+    }
+}
